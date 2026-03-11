2d6f4c4fce92b7437631d6ceff6d27741f112c5f
Jerin <jerinjohnson895@gmail.com>
2026-03-11 15:17:58 +0530

    Colors for the app

--- skillswapapp/app/src/main/java/com/example/skillswap/ui/theme/Color.kt
+++ skillswapapp/app/src/main/java/com/example/skillswap/ui/theme/Color.kt
@@ -9,3 +9,46 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val BrownPrimary = Color(0xFF8B5E3C)
+val BrownDark = Color(0xFF5C3A21)
+val BeigeBackground = Color(0xFFF5EDE4)
+val CreamSurface = Color(0xFFFBF6EF)
+val GoldAccent = Color(0xFFC69A6A)
+val SoftBeigeCard = Color(0xFFEFE2D6)
+val DividerBeige = Color(0xFFE4D6C7)
+
+val TextPrimary = Color(0xFF3B2415)
+val TextSecondary = Color(0xFF6E4A2F)
+val TextHint = Color(0xFFA08975)
+val TitleText = Color(0xFF2B1A0E)
+val CardTitle = Color(0xFF4A2E1B)
+
+val ButtonPrimary = Color(0xFF8B5E3C)
+val ButtonPressed = Color(0xFF6F472D)
+val ButtonSecondary = Color(0xFFC69A6A)
+val ButtonDisabled = Color(0xFFD8C7B6)
+val ButtonDisabledText = Color(0xFF9A8876)
+
+val NavBackground = Color(0xFFFBF6EF)
+val ActiveIcon = Color(0xFF8B5E3C)
+val InactiveIcon = Color(0xFFB79E89)
+
+val SkillTagBackground = Color(0xFFD9BFA6)
+val SkillTagText = Color(0xFF4A2E1B)
+
+val ProgressBarColor = Color(0xFFC69A6A)
+val ProgressTrackColor = Color(0xFFE8D9C8)
+
+val SearchBackground = Color(0xFFFBF6EF)
+val SearchBorder = Color(0xFFE4D6C7)
+val SearchText = Color(0xFF3B2415)
+val SearchIcon = Color(0xFF8B5E3C)
+
+val StarRatingColor = Color(0xFFF2B85B)
+
+val SuccessColor = Color(0xFF6C9A6A)
+val ErrorColor = Color(0xFFB25A4F)
+val WarningColor = Color(0xFFE2A84E)
+val InfoColor = Color(0xFF8B5E3C)
